6a4d70e081db1a9350de7fd2b52d4e17792fd646
shreyansh946 <shreyanshjain648@gmail.com>
2024-01-01 02:07:57 +0530

    this is js file

---
+++ Script.js
@@ -0,0 +1,71 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop1 = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+function toggleVideoStatus()
+{
+    if(video.paused)
+    {
+        video.play();
+    }
+    else
+    {
+        video.pause();
+    }
+}
+
+function updatePlayIcon()
+{
+    if(video.paused)
+    {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    }
+    else{
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+function updateprogress()
+{
+    progress.value = ( video.currentTime /  video.duration ) * 100 ;
+
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10)
+    {
+        mins = '0' + String(mins);
+    }
+
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10)
+    {
+        secs = '0' + String(mins);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+
+}
+
+function setVideoProgress()
+{
+    video.currentTime = (+progress.value * video.duration ) / 100 ;
+}
+
+function stopVideo()
+{
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+video.addEventListener('click',toggleVideoStatus);
+video.addEventListener('pause',updatePlayIcon);
+video.addEventListener('play',updatePlayIcon);
+video.addEventListener('timeupdate',updateprogress);
+
+play.addEventListener('click',toggleVideoStatus);
+
+stop1.addEventListener('click',stopVideo);
+
+progress.addEventListener('change',setVideoProgress);
